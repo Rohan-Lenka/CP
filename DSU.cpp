bd937a3685aa2a3254a8e856c689b3e4bbbc4698
Rohan Lenka <rohanlenka999@gmail.com>
2025-03-17 20:24:14 +0530

    CP folder pushed

---
+++ DSU.cpp
@@ -0,0 +1,33 @@
+class DisjointSet {
+
+public:
+    vector<int> parent, Size;
+    DisjointSet(int v) {
+        Size.resize(v);
+        parent.resize(v);
+        for (int i = 0; i < v; i++) {
+            Size[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    int findPar(int u) {
+        if (parent[u] == u)
+            return u;
+        return parent[u] = findPar(parent[u]);
+    }
+
+    void UnionbySize(int u, int v) {
+        int UP_u = findPar(u);
+        int UP_v = findPar(v);
+        if (UP_u == UP_v)
+            return;
+        if (Size[UP_u] < Size[UP_v]) {
+            parent[UP_u] = UP_v;
+            Size[UP_v] += Size[UP_u];
+        } else {
+            parent[UP_v] = UP_u;
+            Size[UP_u] += Size[UP_v];
+        }
+    }
+};
---
+++ trie.cpp
@@ -0,0 +1,42 @@
+struct Node{
+    Node* links[26];
+    bool flag = false;
+};
+
+class Trie {
+private: 
+Node* root; 
+public:
+    Trie() {
+        root = new Node();   
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(char c : word){
+            if(node->links[c - 'a'] == NULL){
+                node->links[c - 'a'] = new Node();
+            }
+            node = node->links[c - 'a'];
+        }
+        node->flag = true;
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(char c : word){
+            if(!node->links[c - 'a']) return false;
+            node = node->links[c - 'a'];
+        }
+        return node->flag;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(char c : prefix){
+            if(!node->links[c - 'a']) return false;
+            node = node->links[c - 'a'];
+        }
+        return true;
+    }
+};
