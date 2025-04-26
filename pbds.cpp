0441dc716549e184f142a83bb1cb70c9af7e4dc3
Rohan Lenka <rohanlenka999@gmail.com>
2025-04-26 21:32:58 +0530

    slight changes

--- pbds.cpp
+++ pbds.cpp
@@ -3,8 +3,8 @@
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 
-using namespace std;
 using namespace __gnu_pbds;
+using namespace std;
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; // find_by_order, order_of_key
         // data type         // greater / less_equal -> comparator 
