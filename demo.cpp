ad91a1c44b327c979a1da4d588eacda19b30610f
Rohan Lenka <rohanlenka999@gmail.com>
2025-08-21 01:59:32 +0530

    added cp template & cpp.json file for vscode cp snippets in c++

--- demo.cpp
+++ demo.cpp
@@ -1,17 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-void move(int &x, int &y, char &ch) {
-    if(ch == 'U') y--;
-    else if(ch == 'D') y++;
-    else if(ch == 'R') x++;
-    else x--;
-}
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define add push_back
+#define del pop_back
+#define sz(x) ((int)(x).size())
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rrev(x) reverse((x).rbegin(), (x).end())
+#define fori(i, start, limit) for(int i = start; i <= limit; i++)
+#define ford(i, start, limit) for(int i = start; i >= limit; i--)
+
+inline long long myPow(long long a, long long b, long long m=1){long long r=1;while(b){if(b&1)r=(m==1?r*a:r*a%m);a=(m==1?a*a:a*a%m);b>>=1;}return r;}
+inline void yes() { cout << "YES" << '\n'; }
+inline void no() { cout << "NO" << '\n'; }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    string s; cin >> s;
-    int x = 0, y = 0;
-    for(char &ch : s) move(x, y, ch);
-    cout << x << " " << y;
+    
     return 0;
 }
