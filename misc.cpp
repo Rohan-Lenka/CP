7b52b926405ca15edb8eed55cdfb6a9151756a8a
Rohan Lenka <rohanlenka999@gmail.com>
2025-10-11 22:49:26 +0530

    added misc.cpp

---
+++ misc.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool inline intersect(int l1, int r1, int l2, int r2) {
+    return r2 >= l1 && r1 >= l2;
+}
+
+int main() {
+    
+    return 0;
+}
