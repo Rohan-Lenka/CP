879cf723f9f5a6d7e0db5ea71e89e1cbd7f7b3da
Rohan Lenka <rohanlenka999@gmail.com>
2026-05-31 18:41:09 +0530

    added range query & point upd handling for mex in mex.cpp

--- mex.cpp
+++ mex.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 /*
-    TIP: Always think of 0, 1, 2 in most of the mex problems
+    TIP: Think of 0, 1, 2 in some of the mex problems
 */
 
+// 1) Find mex 
 int findMex(vector<int> &a) {
     int n = a.size(), mex = 0;
     vector<bool> f(n + 1, false);
@@ -12,6 +13,48 @@ int findMex(vector<int> &a) {
     while(f[mex]) ++mex; 
     return mex;
 }
+
+// 2) Handle range queries and point updates for mex
+class Mex {
+private:
+    int n;
+    vector<int> a, freq;
+    set<int> missing;
+public:
+    Mex(const vector<int>& arr) {
+        a = arr;
+        n = a.size();
+        freq.assign(n + 2, 0);
+        for(int i = 0; i <= n + 1; i++) {
+            missing.insert(i);
+        }
+        for(int x : a) {
+            if(0 <= x && x <= n + 1) {
+                freq[x]++;
+                missing.erase(x);
+            }
+        }
+    }
+    int getMex() {
+        return *missing.begin();
+    }
+    void update(int idx, int val) {
+        int old = a[idx];
+        if(0 <= old && old <= n + 1) {
+            freq[old]--;
+            if(freq[old] == 0) {
+                missing.insert(old);
+            }
+        }
+        if(0 <= val && val <= n + 1) {
+            if(freq[val] == 0) {
+                missing.erase(val);
+            }
+            freq[val]++;
+        }
+        a[idx] = val;
+    }
+};
 
 int main() {
     
